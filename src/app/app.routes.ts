9e243e89eac92fa92c936956e2ed1b94b103f75c
Stéphane Biehler <biehler@s2i-evolution.eu>
2025-04-12 20:33:52 +0200

    ✨ Added main page
    Added main page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout/layout.component';
-import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: LayoutComponent,
         children: [
-            {path: '', component: AppComponent},
+            {path: '', component: MainComponent},
         ]
         }
 ];
