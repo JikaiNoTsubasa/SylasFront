c44e0c8c00a90e3b28b1899d900659478d3c9120
Stéphane Biehler <biehler@s2i-evolution.eu>
2025-04-12 20:00:18 +0200

    ✨ First layout
    First layout

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout/layout.component';
+import { AppComponent } from './app.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {path: '', component: AppComponent},
+        ]
+        }
+];
---
+++ src/app/layout/layout/layout.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FadeIn } from '../../animations';
+import { HeaderComponent } from '../../comps/header/header.component';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [RouterModule, HeaderComponent],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss',
+  animations: [FadeIn(1000, false)]
+})
+export class LayoutComponent {
+
+}
