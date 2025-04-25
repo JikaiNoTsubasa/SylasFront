3ccde6eb4c90b6ba832b895fdc1697ede3481564
Stéphane Biehler <biehler@s2i-evolution.eu>
2025-04-25 10:40:49 +0200

    ✨ Added Me in settings
    Added Me in settings

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -2,15 +2,28 @@ import { Component, inject } from '@angular/core';
 import { SyService } from '../../Services/SyService';
 import { TabsComponent } from "../../comps/tabs/tabs.component";
 import { TabComponent } from "../../comps/tab/tab.component";
+import { User } from '../../Models/Database/User';
+import { CommonModule } from '@angular/common';
+import { XpBarComponent } from '../../comps/xp-bar/xp-bar.component';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [TabsComponent, TabComponent],
+  imports: [TabsComponent, TabComponent, CommonModule, XpBarComponent],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
 export class SettingsComponent {
 
   syService = inject(SyService);
+
+  meUser: User | null = null;
+
+  ngOnInit(){
+    this.syService.fetchMyUser().subscribe({
+      next: (user) => {
+        this.meUser = user;
+      }
+    });
+  }
 }
